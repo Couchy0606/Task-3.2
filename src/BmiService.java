610e31599f217e37d46578989b7ba87518b413ef
Couchy0606 <irchik669@gmail.com>
2022-08-06 20:08:07 +0100

    changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+
+    public int calculate(int weight, int height) {
+        int result;
+        if (weight > 0) {
+            result = weight / (height * 2 / 100);
+        } else {
+            result = 0;
+        }
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 80;
+        int height = 170;
+        int index = service.calculate(weight, height);
+        System.out.println(index);
+    }
 }
